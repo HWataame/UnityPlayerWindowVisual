abdae1905d87a8f9636afcb625e962f12a1d68a7
hWataame <151874689+HWataame@users.noreply.github.com>
2025-07-25 14:46:06 +0900

    feat: 複数の色を同時に変更する用のパラメーターを実装

--- Script/Libraries/User32Wrapper.cs
+++ Script/Libraries/User32Wrapper.cs
@@ -30,6 +30,18 @@ namespace HW.UnityPlayerWindowVisual.Libraries
         internal delegate bool SingleEnumWindowsProc(
             nint windowHandle, ref SetWindowColorCallbackParameters parameters);
 
+        /// <summary>
+        /// EnumWindowsのコールバック用のデリゲート型
+        /// </summary>
+        /// <remarks>SetWindowColorsCallbackParameterをパラメーターとして渡す用に引数を変更した版</remarks>
+        /// <param name="windowHandle">ウィンドウハンドル</param>
+        /// <param name="parameters">パラメーター</param>
+        /// <returns>列挙を続行するか</returns>
+        [UnmanagedFunctionPointer(CallingConvention.Winapi)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        internal delegate bool MultiEnumWindowsProc(
+            nint windowHandle, ref SetWindowColorsCallbackParameters parameters);
+
 
         /// <summary>
         /// ウィンドウを列挙する
@@ -42,6 +54,18 @@ namespace HW.UnityPlayerWindowVisual.Libraries
         [return: MarshalAs(UnmanagedType.Bool)]
         internal static extern bool EnumWindows(
             SingleEnumWindowsProc callback, ref SetWindowColorCallbackParameters parameters);
+
+        /// <summary>
+        /// ウィンドウを列挙する
+        /// </summary>
+        /// <remarks>SetWindowColorsCallbackParameterをパラメーターとして渡す用に引数を変更した版</remarks>
+        /// <param name="callback">列挙用のコールバック</param>
+        /// <param name="parameters">パラメーター</param>
+        /// <returns>処理結果</returns>
+        [DllImport("user32.dll", EntryPoint = "EnumWindows", CallingConvention = CallingConvention.Winapi)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        internal static extern bool EnumWindows(
+            MultiEnumWindowsProc callback, ref SetWindowColorsCallbackParameters parameters);
 
         /// <summary>
         /// ウィンドウを生成したスレッドとプロセスのIDを取得する
