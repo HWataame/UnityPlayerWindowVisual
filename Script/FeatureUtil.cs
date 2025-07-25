76be503565e734e2ecdf30d8b37e0286e2b612b8
hWataame <151874689+HWataame@users.noreply.github.com>
2025-07-25 14:46:40 +0900

    feat: 複数の色を同時に変更する内部処理を実装

--- Script/FeatureUtil.cs
+++ Script/FeatureUtil.cs
@@ -10,6 +10,7 @@ FeatureUtil.cs
 */
 using AOT;
 using HW.UnityPlayerWindowVisual.Libraries;
+using System;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 using UnityEngine;
@@ -30,6 +31,20 @@ namespace HW.UnityPlayerWindowVisual
         /// SetWindowColorCallbackのデリゲートのキャッシュ
         /// </summary>
         private static readonly User32Wrapper.SingleEnumWindowsProc SetWindowColorCallbackCache;
+        /// <summary>
+        /// SetWindowColorsCallbackのデリゲートのキャッシュ
+        /// </summary>
+        private static readonly User32Wrapper.MultiEnumWindowsProc SetWindowColorsCallbackCache;
+
+        /// <summary>
+        /// 色の種類に対応する属性値
+        /// </summary>
+        private static readonly uint[] ColorTypeAttributes =
+        {
+            WindowBorderColor.WindowBorderColorAttribute,
+            WindowTitlebarColor.DwmWindowCaptionColor,
+            WindowTitlebarTextColor.DwmWindowCaptionTextColor
+        };
 #endif
 
         /// <summary>
@@ -63,6 +78,9 @@ namespace HW.UnityPlayerWindowVisual
 
             // SetWindowColorCallbackのデリゲートをキャッシュする
             SetWindowColorCallbackCache = SetWindowColorCallback;
+
+            // SetWindowColorsCallbackのデリゲートをキャッシュする
+            SetWindowColorsCallbackCache = SetWindowColorsCallback;
 #endif
         }
 
@@ -135,6 +153,93 @@ namespace HW.UnityPlayerWindowVisual
             DwmApiWrapper.DwmSetWindowAttribute(
                 windowHandle, parameters.AttributeType,
                 ref colorValue, DwmApiWrapper.WindowColorValueDataSize);
+
+            return true;
+        }
+#endif
+
+        /// <summary>
+        /// ウィンドウの複数の色を設定する(内部処理)
+        /// </summary>
+        /// <param name="type">設定する色のフラグ</param>
+        /// <param name="borderColorValue">縁の色を示す値</param>
+        /// <param name="titlebarColorValue">タイトルバーの色を示す値</param>
+        /// <param name="titlebarTextColorValue">タイトルバーの文字色を示す値</param>
+        /// <param name="typeName">型名</param>
+        /// <param name="kindName">値の種類名</param>
+        /// <returns>処理結果</returns>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        internal static bool SetInternal(
+            WindowColorType type, uint borderColorValue, uint titlebarColorValue,
+            uint titlebarTextColorValue, string typeName, string kindName)
+        {
+#if UNITY_EDITOR_WIN || UNITY_STANDALONE_WIN
+            if (isAllowProcess)
+            {
+                // 実行が許可される場合
+                // パラメーターを準備する
+                var parameters = new SetWindowColorsCallbackParameters(
+                    type, borderColorValue, titlebarColorValue,
+                    titlebarTextColorValue, Kernel32Wrapper.GetCurrentProcessId());
+
+                // トップレベルのウィンドウを処理する
+                return User32Wrapper.EnumWindows(SetWindowColorsCallbackCache, ref parameters);
+            }
+            else
+            {
+                // 実行が許可されない(WindowsのUnityEditor)場合は失敗
+                if (isOutputLog)
+                {
+                    Debug.LogWarning($"[UnityPlayerWindowVisual.{typeName ?? "(不明な型)"}] " +
+                        "Windowsのスタンドアロンプレイヤー以外の環境" +
+                        $"({Application.platform})から{kindName ?? "(不明値)"}を設定しようとしました");
+                }
+                return false;
+            }
+#else
+            // Windows環境以外である場合は失敗
+            if (isOutputLog)
+            {
+                Debug.LogWarning($"[UnityPlayerWindowVisual.{typeName ?? "(不明な型)"}] " +
+                    $"Windows以外の環境({Application.platform})から{kindName ?? "(不明値)"}を設定しようとしました");
+            }
+            return false;
+#endif
+        }
+
+#if UNITY_EDITOR_WIN || UNITY_STANDALONE_WIN
+        /// <summary>
+        /// ウィンドウの複数の色を設定する処理のコールバック関数
+        /// </summary>
+        /// <param name="windowHandle">ウィンドウハンドル</param>
+        /// <param name="parameters">パラメーター</param>
+        /// <returns>処理を続行するか</returns>
+        [MonoPInvokeCallback(typeof(User32Wrapper.MultiEnumWindowsProc))]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        private static bool SetWindowColorsCallback(
+            nint windowHandle, ref SetWindowColorsCallbackParameters parameters)
+        {
+            if (User32Wrapper.GetWindowThreadProcessId(windowHandle, out var processId) == 0 ||
+                parameters.ProcessId != processId)
+            {
+                // 現在のプロセスIDとウィンドウを生成したプロセスのIDが異なる場合は何もしない
+                return true;
+            }
+
+            // 属性値を取得する
+            ReadOnlySpan<uint> attributes = ColorTypeAttributes;
+
+            // ウィンドウの色を設定する
+            for (int i = 0; i < attributes.Length; ++i)
+            {
+                var (isSet, colorValue) = parameters[i];
+                if (isSet)
+                {
+                    // 処理中の色が設定されている場合はウィンドウの色を設定する
+                    DwmApiWrapper.DwmSetWindowAttribute(windowHandle, attributes[i],
+                        ref colorValue, DwmApiWrapper.WindowColorValueDataSize);
+                }
+            }
 
             return true;
         }
