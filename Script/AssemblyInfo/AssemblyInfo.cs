55da56d84b73378a2f4205d22bb952a0006fc51e
hWataame <151874689+HWataame@users.noreply.github.com>
2025-07-25 10:26:33 +0900

    add: パッケージ情報を追加

---
+++ Script/AssemblyInfo/AssemblyInfo.cs
@@ -0,0 +1,19 @@
+/*
+━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+アセンブリ情報を保持するファイル
+
+AssemblyInfo.cs
+────────────────────────────────────────
+バージョン: 1.0.0
+2025 Wataame(HWataame)
+━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+*/
+using System.Reflection;
+
+[assembly: AssemblyTitle("UnityPlayer Window Visual")]
+[assembly: AssemblyProduct("UnityPlayer Window Visual")]
+[assembly: AssemblyCompany("Wataame(HWataame)")]
+[assembly: AssemblyCopyright("2025 Wataame(HWataame)")]
+[assembly: AssemblyVersion("1.0.0")]
+[assembly: AssemblyFileVersion("1.0.0")]
+[assembly: AssemblyInformationalVersion("1.0.0")]
